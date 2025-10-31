2391a7e0d7a7c8dd8141345ce8c0466566de10bc
Ian Watson <iwatson@inkey.tech>
2025-10-31 07:52:15 +0200

    Update API URL in PHPSample1.php

--- http-api/php/PHPSample1.php
+++ http-api/php/PHPSample1.php
@@ -3,7 +3,7 @@
 
 //DISCLAIMER: The sample code is provided as a guideline to demonstrate how our API works. The code is provided "as is" without any guarantees. The sample code has been tested and works in our environment, results may vary in different system configurations and software programs therefore it is expected that you have sufficient technical knowledge and development skills to address any coding issues that may arise from using this sample code.
 
-$url = "https://www.winsms.co.za/api/batchmessage.asp?";
+$url = "https://api.winsms.co.za/api/batchmessage.asp?";
 
 $userp = "user=";
 
@@ -48,5 +48,6 @@ Ensure that fopen_allow_url is set to true in php.ini (it is by default).
 
 If the web server is behind a firewall or proxy server, you will have to open it for the php script, or alternatively look into setting the proxy settings for authentication in the .php script.
 */
+
 
 ?>
